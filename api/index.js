9f10d096b6539d10a97b2ffc75ee7da8e8aad7b0
aceblack0003 <aceblack0003@gmail.com>
2023-04-22 16:14:49 +0700

    update

--- api/index.js
+++ api/index.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
+
+app.use(express.json())
+app.use(cors({
+    credentials: true,
+    origin: 'http://127.0.0.1:5173',
+}))
 
 app.get('/test', (req, res) => {
     res.json('test ok')
 })
 
-app.listen(4000);
+app.post('/register', (req, res) => {
+    const { name, password, email } = req.body
+    res.json({ name, password, email })
+})
+
+app.listen(4000); 
